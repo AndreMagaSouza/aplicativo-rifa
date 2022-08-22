a2f27cf8a9ba27c342683a48c614772dad5af87f
Jamgf <jmfilh@gmail.com>
2022-08-22 13:11:58 -0300

    teste1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: RifaApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-
+class RifaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RifaApp'),
+        backgroundColor: Colors.cyan,
+      ),
+      backgroundColor: Colors.white38,
+      body: Center(
+          child: Text(
+        'Texto da Rifa aqui',
+        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.arrow_forward),
+      ),
+    );
   }
 }
